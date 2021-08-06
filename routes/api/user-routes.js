0b3a7d680d7a331d35c203472cff2b109f196e38
tannerlsmith <tanner.smith@uvm.edu>
2021-08-06 09:23:09 -0400

    password showing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,11 +50,31 @@ router.post('/', (req, res) => {
     });
 });
 
+// LOGIN Route
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        // res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData });
+  
+      // Verify user
+  
+  });  
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-  
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
