b92729553eaf79c75d08019223d71905f9b3e7e8
tannerlsmith <tanner.smith@uvm.edu>
2021-08-06 09:47:18 -0400

    private password, login routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -59,14 +59,18 @@ router.post('/login', (req, res) => {
       }
     }).then(dbUserData => {
       if (!dbUserData) {
-        // res.status(400).json({ message: 'No user with that email address!' });
+        res.status(400).json({ message: 'No user with that email address!' });
         return;
       }
-  
-      res.json({ user: dbUserData });
-  
+      
       // Verify user
-  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
   });  
 });
 
