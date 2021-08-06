56ba7cc6194ac8972a2a20adfffd3c6a08f0743a
tannerlsmith <tanner.smith@uvm.edu>
2021-08-05 22:53:03 -0400

    updated user routes exclude pw

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('literary_times_db', 'username', 'password', {
-    host: 'localhost', 
-    dialect: 'mysql',
-    port: 3306
-})
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,31 +4,34 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll()
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
+      });
 });
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-    User.findOne({
-      where: {
-        id: req.params.id
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
       }
+      res.json(dbUserData);
     })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
@@ -49,7 +52,6 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
