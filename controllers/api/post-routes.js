b3405a1d77734a864ead599a147fa539298ab38a
tannerlsmith <tanner.smith@uvm.edu>
2021-08-06 21:56:19 -0400

    line 14 error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -76,7 +76,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
